d5f14bffe47ec1b66473e61c2756bf4ecd056bbb
061019-top <chudinhminh21@gmail.com>
2024-11-21 09:51:26 +0700

    BTVN SS2-5

---
+++ BT5 SS2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int dai = 10;
+    int rong = 5;
+    int chuvi = 2 * (dai + rong);
+    int dientich = dai * rong;
+    printf("Chieu dai cua hinh chu nhat la: %d\n", dai);
+    printf("Chieu rong cua hinh chu nhat la: %d\n", rong);
+    printf("Chu vi cua hinh chu nhat la: %d\n", chuvi);
+    printf("Dien tich cua hinh chu nhat la: %d\n", dientich);
+    return 0;
+}
+
