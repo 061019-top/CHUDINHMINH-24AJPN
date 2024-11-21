dfb9083307d96a895149849407203887681c3498
061019-top <chudinhminh21@gmail.com>
2024-11-21 09:51:04 +0700

    BTVN SS2-4

---
+++ BT4 SS2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int canh = 5;
+    int chuvi = 4 * canh;
+    int dientich = canh * canh;
+    printf("Canh cua hinh vuong là: %d\n", canh);
+    printf("Chu vi cua hinh vuong là: %d\n", chuvi);
+    printf("Dien tich cua hinh vuong là: %d\n", dientich);
+    return 0;
+}
+
