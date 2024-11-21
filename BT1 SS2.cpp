1ca912855b1c28d5b06417398b466843d76baaa3
061019-top <chudinhminh21@gmail.com>
2024-11-21 09:48:34 +0700

    BTVN SS2-1

---
+++ BT1 SS2.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    // Kieu int (so nguyên)
+    int a = 10; // Khai bao và khoi tao bien a voi gia tri 10
+    
+    // Kieu float (so thuc)
+    float b = 3.14; // Khai bao và khoi tao bien b voi gia tri 3.14
+    
+    // Kieu double (so thuc có do chính xác cao)
+    double c = 3.141592653589793; // Khai báo và khoi tao bien c voi giá tri chính xác hon
+    
+    // Kieu char (ki tu)
+    char d = 'A'; // Khai báo và khoi tao bien d voi giá tri ki tu 'A'
+    
+    return 0;
+}
+
