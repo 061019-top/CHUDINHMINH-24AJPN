507a6867912f660e11992935a122b2abd20e2c0c
061019-top <chudinhminh21@gmail.com>
2024-11-21 09:50:26 +0700

    BTVN SS2-3

---
+++ BT3 SS2.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+    int b = 5;
+    int sum = a + b;
+    int Hieu = a - b;
+    int Tich = a * b;
+    int Thuong = a / b;
+    printf("Tong cua %d và %d là: %d\n", a, b, sum);
+    printf("Hieu cua %d và %d là: %d\n", a, b, Hieu);
+    printf("Tich cua %d và %d là: %d\n", a, b, Tich);
+    printf("Thuong cua %d và %d là: %d\n", a, b, Thuong);
+    return 0;
+}
+
