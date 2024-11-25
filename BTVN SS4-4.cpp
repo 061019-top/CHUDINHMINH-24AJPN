157df16163a6310713989dcdf2a3d0594644bce9
061019-top <chudinhminh21@gmail.com>
2024-11-25 08:30:59 +0700

    Update BTVN SS4-4.cpp

--- BTVN SS4-4.cpp
+++ BTVN SS4-4.cpp
@@ -3,11 +3,9 @@
 int main() {
     int month;
 
-    // Yeu cau nguoi dung nhap vao so thang
     printf("Vui long nhap vao so thang (1-12): ");
     scanf("%d", &month);
-
-    // Kiem tra so thang va hien thi ket qua
+    
     switch (month) {
         case 1: case 3: case 5: case 7: case 8: case 10: case 12:
             printf("Thang %d co 31 ngay.\n", month);
