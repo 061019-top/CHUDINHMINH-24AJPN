d33e4506607b446aef7a364903626b3025f7b36c
061019-top <chudinhminh21@gmail.com>
2024-12-02 16:59:09 +0700

    BTVN SS7-4

---
+++ BTVN SS7-4.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Nhap so phan tu cua mang: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Nhap cac phan tu cua mang:\n");
+    for (int i = 0; i < n; i++) {
+        printf("Phan tu thu %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Cac phan tu trong mang la:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
+
