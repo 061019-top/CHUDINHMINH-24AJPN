4e7baf1e41c3b33bc35049e42c87455730f94682
061019-top <chudinhminh21@gmail.com>
2024-12-12 08:01:47 +0700

    BTVN SS14-3

---
+++ BTVN SS14-3.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char chuoi[] = "Xin chao!";
+
+    int doDai = strlen(chuoi);
+    printf("Chuoi goc: %s\n", chuoi);
+
+    printf("Chuoi dao nguoc: ");
+    for (int i = doDai - 1; i >= 0; i--) {
+        printf("%c", chuoi[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
+
