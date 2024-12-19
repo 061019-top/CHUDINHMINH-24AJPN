8d6f3d3a3863b05a1dbc228a00a4e024ccda22a4
061019-top <chudinhminh21@gmail.com>
2024-12-20 06:11:25 +0700

    BTVN SS18-2

---
+++ BTVN SS18-2.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+// Dinh nghia cau truc Student
+struct Student {
+    char name[50];        
+    int age;              
+    char phoneNumber[15]; 
+};
+
+int main() {
+    struct Student student1;
+
+    printf("Nhap ten: ");
+    scanf("%49s", student1.name); 
+    printf("Nhap tuoi: ");
+    scanf("%d", &student1.age);
+    printf("Nhap so dien thoai: ");
+    scanf("%14s", student1.phoneNumber); 
+    printf("\nThong tin sinh vien:\n");
+    printf("Ten: %s\n", student1.name);
+    printf("Tuoi: %d\n", student1.age);
+    printf("So dien thoai: %s\n", student1.phoneNumber);
+
+    return 0;
+}
+
